7d25e5dd4f5d82d2c345f7305db8afaf4b5a9d82
Theresa Eimer <eimer@tnt.uni-hannover.de>
2023-04-19 13:46:21 +0200

    sb3 to train

--- rl_exercises/train_agent.py
+++ rl_exercises/train_agent.py
@@ -1,14 +1,30 @@
 import hydra
+import numpy as np
 import gymnasium as gym
 from gymnasium.wrappers import TimeLimit
 import compiler_gym
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3 import SAC
 
 
 @hydra.main("rl_exercises/configs", "base", version_base="1.1")
 def train(cfg):
     env = make_env(cfg.env_name)
     if cfg.agent == 'sb3':
-        pass
+        # Create agent
+        model = SAC("MlpPolicy", env, verbose=cfg.verbose, tensorboard_log=cfg.log_dir, seed=cfg.seed)
+
+        # Train agent
+        model.learn(total_timesteps=cfg.total_timesteps)
+
+        # Save agent
+        model.save(cfg.model_fn)
+
+        # Evaluate
+        env = Monitor(gym.make(cfg.env_id))
+        means = evaluate(env, model, n_episodes=cfg.n_eval_episodes)
+        performance = np.mean(means)
+        return performance
     else:
         # TODO: add your agent options here
         raise NotImplementedError
@@ -29,6 +45,8 @@ def make_env(env_name):
         env = TimeLimit(env, max_episode_steps=100)
     else:
         env = gym.make(env_name)
+    env = Monitor(env)
+    return env
 
 
 if __name__ == "__main__":
