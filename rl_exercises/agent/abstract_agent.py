dd6e279ceaf5a9140deca284b983794e6ccb45a9
benjamc <benjamin@tnt.uni-hannover.de>
2023-04-19 14:25:52 +0200

    Add args+kwargs

--- rl_exercises/agent/abstract_agent.py
+++ rl_exercises/agent/abstract_agent.py
@@ -1,26 +1,25 @@
 from __future__ import annotations
 
 import numpy as np
+from typing import Any
 from abc import abstractmethod
 
 class AbstractAgent(object):
-    def __init__(self) -> None:
+    def __init__(self, *args, **kwargs) -> None:
         pass
 
     @abstractmethod
-    def predict(
-        observation
-    ):
+    def predict(*args, **kwargs) -> Any:
         ...
 
     @abstractmethod
-    def save():
+    def save(*args, **kwargs) -> Any:
         ...
 
     @abstractmethod
-    def load():
+    def load(*args, **kwargs) -> Any:
         ...
 
     @abstractmethod
-    def update():
+    def update(*args, **kwargs) -> Any:
         ...
