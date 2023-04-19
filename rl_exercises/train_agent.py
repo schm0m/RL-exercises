f8360b6947eef977cf8946d1e46fc2d835f06b3d
Theresa Eimer <eimer@tnt.uni-hannover.de>
2023-04-19 13:42:32 +0200

    progress on central train/eval

--- rl_exercises/train_agent.py
+++ rl_exercises/train_agent.py
@@ -1,9 +1,12 @@
 import hydra
 import gymnasium as gym
+from gymnasium.wrappers import TimeLimit
+import compiler_gym
+
 
 @hydra.main("rl_exercises/configs", "base", version_base="1.1")
 def train(cfg):
-    env = gym.make(cfg.env_name)
+    env = make_env(cfg.env_name)
     if cfg.agent == 'sb3':
         pass
     else:
@@ -17,7 +20,16 @@ def train(cfg):
             evaluate(env, policy, cfg.n_eval_episodes)
 
 def evaluate(env, policy, n_episodes):
-    pass
+    raise NotImplementedError
+
+def make_env(env_name):
+    if "compiler" in env_name:
+        benchmark = "cbench-v1/dijkstra"
+        env = gym.make("llvm-autophase-ic-v0", benchmark=benchmark, reward_space="IrInstructionCountNorm", apply_api_compatibility=True)
+        env = TimeLimit(env, max_episode_steps=100)
+    else:
+        env = gym.make(env_name)
+
 
 if __name__ == "__main__":
     train()
