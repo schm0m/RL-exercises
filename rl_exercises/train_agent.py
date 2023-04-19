acb4bf06a2c29434227ef00be7ae7db3adce13c7
Theresa Eimer <eimer@tnt.uni-hannover.de>
2023-04-19 14:11:43 +0200

    Train loop

--- rl_exercises/train_agent.py
+++ rl_exercises/train_agent.py
@@ -11,29 +11,55 @@ from stable_baselines3 import SAC
 def train(cfg):
     env = make_env(cfg.env_name)
     if cfg.agent == 'sb3':
-        # Create agent
-        model = SAC("MlpPolicy", env, verbose=cfg.verbose, tensorboard_log=cfg.log_dir, seed=cfg.seed)
-
-        # Train agent
-        model.learn(total_timesteps=cfg.total_timesteps)
-
-        # Save agent
-        model.save(cfg.model_fn)
-
-        # Evaluate
-        env = Monitor(gym.make(cfg.env_id))
-        means = evaluate(env, model, n_episodes=cfg.n_eval_episodes)
-        performance = np.mean(means)
-        return performance
+        return train_sb3_sac(cfg)
     else:
         # TODO: add your agent options here
         raise NotImplementedError
     
+    agent = None
+    TODO: make buffer/trajectory
+    state, info = env.reset()
+
+    num_episodes = 0
     for step in range(cfg.training_steps):
-        pass
-        
+        action = agent.predict(state)
+        next_state, reward, terminated, truncated, info = env.step(action)
+
+        if buffer is not None:
+            buffer.add(state, action, reward, next_state, (truncated or terminated), info)
+
+        if len(buffer) > cfg.batch_size or (cfg.update_after_episode_end and (terminated or truncated)):
+            batch = buffer.sample(cfg.batch_size)
+            agent.update(batch)
+
+        state = next_state
+
+        if terminated or truncated:
+            state = env.reset()
+
         if step % cfg.eval_every_n_steps == 0:
-            evaluate(env, policy, cfg.n_eval_episodes)
+            eval_performance = evaluate(env, policy, cfg.n_eval_episodes)
+            print(f"Eval reward after {step} steps was {eval_performance}.")
+    
+    final_eval = evaluate(env, policy, cfg.n_eval_episodes)
+    print(final_eval)
+    return final_eval
+
+def train_sb3_sac(cfg):
+    # Create agent
+    model = SAC("MlpPolicy", env, verbose=cfg.verbose, tensorboard_log=cfg.log_dir, seed=cfg.seed)
+
+    # Train agent
+    model.learn(total_timesteps=cfg.total_timesteps)
+
+    # Save agent
+    model.save(cfg.model_fn)
+
+    # Evaluate
+    env = Monitor(gym.make(cfg.env_id))
+    means = evaluate(env, model, n_episodes=cfg.n_eval_episodes)
+    performance = np.mean(means)
+    return performance
 
 def evaluate(env, policy, n_episodes):
     raise NotImplementedError
