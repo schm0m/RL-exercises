90ccb3ce625d08832eb3c3074625a6c7c962e6df
amsks <adityak735@gmail.com>
2023-05-05 11:01:12 +0200

    formatted with black

--- rl_exercises/week_4/sarsa.py
+++ rl_exercises/week_4/sarsa.py
@@ -5,17 +5,11 @@ from typing import Callable, DefaultDict, Hashable, List, Tuple
 import gymnasium as gym
 import numpy as np
 
+
 class SARSA(object):
-    """ SARSA algorithm """
-    
-    def __init__ (
-        self,
-        env: gym.Env,
-        num_episodes: int, 
-        gamma: float = 1.0, 
-        alpha: float = 0.5, 
-        epsilon: float = 0.1
-    ):
+    """SARSA algorithm"""
+
+    def __init__(self, env: gym.Env, num_episodes: int, gamma: float = 1.0, alpha: float = 0.5, epsilon: float = 0.1):
         """Initialize the SARSA agent
 
         Parameters
@@ -31,20 +25,20 @@ class SARSA(object):
         epsilon : float, optional
             Exploration Parameter, by default 0.1
         """
-        
+
         # Check hyperparameter boundaries
         assert 0 <= gamma <= 1, "Gamma should be in [0, 1]"
         assert 0 <= epsilon <= 1, "epsilon has to be in [0, 1]"
         assert alpha > 0, "Learning rate has to be greater than 0"
-        
-        self.env  = env
-        self.num_episodes= num_episodes
+
+        self.env = env
+        self.num_episodes = num_episodes
         self.gamma = gamma
         self.alpha = alpha
-        self. epsilon = epsilon
-        
+        self.epsilon = epsilon
+
         self.n_actions = self.env.action_space.n
-        
+
         # create Q structure
         self.Q: DefaultDict[int, np.ndarray] = defaultdict(lambda: np.zeros(self.n_actions))
 
@@ -61,23 +55,22 @@ class SARSA(object):
         int
             Index of chosen action
         """
-        
+
         # TODO: Implement a Epsilon-Greedy policy
-        
+
         if np.random.random() > 1 - epsilon:
             p = np.random.random(self.n_actions)
         else:
             p = np.zeros(self.n_actions)
             p[np.where(Q[state] == np.max(Q[state]))] = 1
-        
+
         # Make a probability distribution
         probs = p / np.sum(p)
-        
+
         # Choose action from a probability distribution
         np.random.choice(range(len(p)), p=probs)
-        
-        return 0
 
+        return 0
 
     def update(
         Q: DefaultDict[int, np.ndarray],
@@ -93,7 +86,7 @@ class SARSA(object):
         Parameters
         ----------
         Q : DefaultDict[int, np.ndarray]
-            List of Q values 
+            List of Q values
         state : int
             [description]
         action : int
