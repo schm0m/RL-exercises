02df4b15f8171563708ae23bed6eae3c3662e88f
benjamc <c.benjamins@ai.uni-hannover.de>
2023-06-16 16:46:05 +0200

    Fix imports

---
+++ rl_exercises/week_4/__init__.py
@@ -0,0 +1,5 @@
+from rl_exercises.week_4.sarsa import EpsilonGreedyPolicy
+
+__all__ = [
+    "EpsilonGreedyPolicy"
+]
---
+++ rl_exercises/week_5/__init__.py
@@ -0,0 +1,6 @@
+from rl_exercises.week_5.q_learning_tabular import TabularQAgent
+from rl_exercises.week_5.q_learning_vfa import VFAQAgent
+
+__all__ = [
+    "VFAQAgent"
+]
