e996c58e0da6cecad9c213f66a50f083a8c0a80d
benjamc <c.benjamins@ai.uni-hannover.de>
2023-07-03 15:43:07 +0200

    Add tests

--- tests/week_2/test_policy_iteration.py
+++ tests/week_2/test_policy_iteration.py
@@ -2,7 +2,7 @@ import unittest
 import numpy as np
 
 from rl_exercises.environments import MarsRover
-from rl_exercises.week_2.policy_iteration import PolicyIteration
+from rl_exercises.week_2.policy_iteration import PolicyIteration, do_policy_improvement
 from rl_exercises.train_agent import evaluate
 
 
@@ -27,38 +27,18 @@ class TestPolicyIteration(unittest.TestCase):
             steps.append(agent.steps)
 
         self.assertTrue(np.mean(steps) > 1)   
-        self.assertTrue(sum(r) > 0)     
+        self.assertTrue(sum(r) > 0)    
 
-    # def test_single_update(self):
-    #     new_qs, new_pi, converged = update_policy([[0, 0], [0, 0]], [0, 1], 0, 1, 1, 10)
-    #     self.assertTrue(new_pi[0] == 1)
-    #     self.assertTrue(new_qs[0][1] > 0)
-    #     self.assertFalse(converged)
-
-    # def test_update_consistency(self):
-    #     for _ in range(10):
-    #         pos = np.random.randint(7) + 1
-    #         qs = np.random.randint(5, size=(8, 2))
-    #         new_qs, new_pi, _ = update_policy(
-    #             qs,
-    #             np.random.randint(2, size=8),
-    #             pos,
-    #             pos + (1 * -np.random.randint(2)),
-    #             np.random.randint(2),
-    #             np.random.randint(10),
-    #         )
-    #         self.assertTrue(
-    #             all(
-    #                 [
-    #                     np.any(np.where(new_qs[state] == new_qs[state].max()) == new_pi[state])
-    #                     for state in np.arange(len(new_qs))
-    #                 ]
-    #             )
-    #         )
-
-    # def test_convergence(self):
-    #     _, _, converged = update_policy([[1, 0], [1, 0]], [0, 0], 0, 1, 1, 0)
-    #     self.assertTrue(converged)
+    def test_policy_improvement(self):
+        Q = np.array([
+            [1, 0],
+            [0, 1],
+            [1, 0]
+        ])
+        pi_before = [0, 1, 0]
+        pi, converged = do_policy_improvement(Q, pi_before) 
+        self.assertTrue(np.all(pi_before == pi))
+        self.assertTrue(converged)
 
 
 if __name__ == "__main__":
