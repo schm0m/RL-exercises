ff201d602b74f9a14f1e2896d3a35ca819cc1138
Lukas Fehring <lukas.fehring@stud.uni-hannover.de>
2023-09-22 14:51:12 +0200

    Add minor docs to abstract agent

--- rl_exercises/agent/abstract_agent.py
+++ rl_exercises/agent/abstract_agent.py
@@ -10,17 +10,21 @@ class AbstractAgent(object):
 
     @abstractmethod
     def predict(self, *args: tuple[Any], **kwargs: dict) -> tuple[Any, dict]:
+        """Predict action based on observation."""
         ...
 
     @abstractmethod
     def save(self, *args: tuple[Any], **kwargs: dict) -> Any:
+        """Save agent to the disk."""
         ...
 
     @abstractmethod
     # TODO what is the return type? A callable?
     def load(self, *args: tuple[Any], **kwargs: dict) -> Any:
+        """Load agent from the disk."""
         ...
 
     @abstractmethod
     def update(self, *args: tuple[Any], **kwargs: dict) -> Any | None:
+        """Td update of the agent."""
         ...
